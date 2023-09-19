2aad1d23ce0122f75458be5d504d3538457e7ad4
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-09-19 21:50:42 +0300

    Add project files.

---
+++ ArrayTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the elements of the array:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Element {i + 1}: ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Elements stored in the array:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Element {i + 1}: {arr[i]}");
+        }
+    }
+}
